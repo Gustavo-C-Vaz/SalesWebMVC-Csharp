017186d4269c10b2afa289499ae71c6e8d7152f9
Gustavo Carvalho Vaz <gut_ogut@hotmail.com>
2025-03-06 13:36:43 -0300

    Seeding Service

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,26 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
+using SalesWebMVC.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SalesWebMVCContext>(options =>
     options.UseMySql(builder
     .Configuration
-    .GetConnectionString("SalesWebMVCContext"
-),
-    ServerVersion.AutoDetect(builder
-    .Configuration
-    .GetConnectionString("SalesWebMVCContext"
-))));
-
-
+    .GetConnectionString("SalesWebMVCContext"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMVCContext"))));
 //options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found.")));
-
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -30,8 +25,17 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var seedingService = new SeedingService(services.GetRequiredService<SalesWebMVCContext>());
+seedingService.Seed();
+
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+//app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();  Não funcionou sozinho!!
+// Seeding service que estaria no Start.cs
 
 
 app.UseRouting();
@@ -39,7 +43,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
