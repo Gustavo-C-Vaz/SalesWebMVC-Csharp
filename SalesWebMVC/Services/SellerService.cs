de67b3477d04bf7856857b007e95749255db1abb
Gustavo Carvalho Vaz <gut_ogut@hotmail.com>
2025-03-07 15:33:59 -0300

    Foreign key not null (referential integrity)

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -16,6 +16,7 @@ namespace SalesWebMVC.Services
         }
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First(); // SOLUÇÃO PROVISÓRIA PARA INSERIR SELLER!
             _context.Add(obj);
             _context.SaveChanges();
         }
