a960fcf15f4892fe0ced7024372ce33f75d91ef3
Gustavo Carvalho Vaz <gut_ogut@hotmail.com>
2025-03-07 10:48:37 -0300

    SellerService and basic FindAll

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,18 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using SalesWebMVC.Controllers;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<SalesWebMVCContext>(options =>
     options.UseMySql(builder
     .Configuration
     .GetConnectionString("SalesWebMVCContext"),
-    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMVCContext"))));
-//options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found.")));
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMVCContext")))); // MySQL -------
+//options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMVCContext")
+//?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<SellerService>();  // SellerService -----
 
 var app = builder.Build();
 
@@ -28,20 +33,13 @@ if (!app.Environment.IsDevelopment())
 var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 var seedingService = new SeedingService(services.GetRequiredService<SalesWebMVCContext>());
-seedingService.Seed();
-
+seedingService.Seed();  // SeedingService -----------
 
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-//app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();  Não funcionou sozinho!!
-// Seeding service que estaria no Start.cs
-
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
 name: "default",
 pattern: "{controller=Home}/{action=Index}/{id?}");
